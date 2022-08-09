3a467fd8f4b85086e17de88d4d4c870264ea9794
ivmerk <ivan.e.merkulov@gmail.com>
2022-08-09 16:25:09 +0300

    babel webpack installed

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,13 @@ const path = require('path');
        patterns: [{ from: 'public' }],
      }),
    ],
+   module: {
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+        }
+    ]
+  }
  };
