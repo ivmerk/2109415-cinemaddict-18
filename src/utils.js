343c4ee97293345cced0751bbb474402727e9c58
ivmerk <ivan.e.merkulov@gmail.com>
2022-08-20 11:35:22 +0300

    fixed

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 import dayjs from 'dayjs';
 
-const duration = require('dayjs/plugin/duration');
-dayjs.extend(duration);
+//dayjs.extend(duration);
 // Функция из интернета по генерации случайного числа из диапазона
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 const getRandomInteger = (a = 0, b = 1) => {
@@ -26,7 +25,8 @@ const formatStringToDate = (date) => dayjs(date).format('DD MMMM YYYY');
 
 const formatStringToYear = (date) => dayjs(date).format('YYYY');
 
-const formatMinutesToTime = (minutes) => dayjs.duration(minutes, 'minutes').format('H[h] mm[m]');
+const formatMinutesToTime = (date) => dayjs(date).format('H[h] mm[m]');
+// const formatMinutesToTime = (minutes) => dayjs.duration(minutes, 'minutes').format('H[h] mm[m]');
 
 
 export { getRandomInteger, getRandomValue, getRandomFloat, formatStringToDateWithTime, formatStringToDate, formatStringToYear, formatMinutesToTime };
