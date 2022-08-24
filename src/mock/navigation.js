632b5efc35832da9c352168b0ae379c41cbfad18
ivmerk <ivan.e.merkulov@gmail.com>
2022-08-24 20:51:05 +0300

    4.2. 1morefixed

--- src/mock/navigation.js
+++ src/mock/navigation.js
@@ -2,11 +2,5 @@ import { NAVIGATIONFILTERTYPES } from '../const.js';
 import { getRandomInteger } from '../utils.js';
 
 
-// const createNavigationItem = (item) => ({
-//   type: NAVIGATIONFILTERTYPES[item],
-//   quantity: getRandomInteger(100),
-// });
-
-
 export const generateNavigationList = () =>
-  NAVIGATIONFILTERTYPES.reduce((obj, item, index) => { obj[item] = getRandomInteger(100); return obj; }, {});
+  NAVIGATIONFILTERTYPES.reduce((obj, item) => { obj[item] = getRandomInteger(100); return obj; }, {});
