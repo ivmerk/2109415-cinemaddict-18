106a953f51cd2c0f98b9be652711f99b12fae289
ivmerk <ivan.e.merkulov@gmail.com>
2022-09-26 21:30:39 +0300

    5.9 done

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import CommentsModel from './model/comments-model.js';
 import FilterModel from './model/filter-model.js';
 
 import FilmsApiService from './api-services/films-api-service.js';
-import CommentsApiService from './api-services/comments-api-service.js';
+// import CommentsApiService from './api-services/comments-api-service.js';
 
 const AUTHORIZATION = 'Basic $er883jdzbdw';
 const END_POINT = 'https://18.ecmascript.pages.academy/cinemaddict/';
